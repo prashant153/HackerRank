db33af36d6750a2c05b5ba641901376250b5197f
Prashant <prashant@cis001.local>
2017-06-29 16:16:35 +0530

    Works fine for large input

--- c#/DataStructure/Arrays/DynamicArray.cs
+++ c#/DataStructure/Arrays/DynamicArray.cs
@@ -1,28 +1,18 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace Rextester
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            string[] arr_temp = Console.ReadLine().Split(' ');
-            int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
-            int N = arr[0];
-            int[] S = new int[]{0,0,0};
-            int Q = arr[1];
-            int lastAnswer = 0;
-            List<int>[] list=new List<int>[100];
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        string[] arr_temp = Console.ReadLine().Split(' ');
+            long[] arr = Array.ConvertAll(arr_temp,long.Parse);
+            long N = arr[0];
+            long Q = arr[1];
+            long lastAnswer = 0;
+            List<long>[] list=new List<long>[N];
             for(int i=0;i<N;i++)
             {
-                list[i] = new List<int>();
+                list[i] = new List<long>();
             }
             for(int i=0;i<Q;i++)
             {
@@ -30,17 +20,16 @@ namespace Rextester
                 int[] qry = Array.ConvertAll(qry_temp,Int32.Parse);
                 if(qry[0]==1)
                 {
-                    list[(qry[1] ^ lastAnswer)%N]=list[(qry[1] ^ lastAnswer)%N].Add(qry[2]);
+                    list[(qry[1] ^ lastAnswer)%N].Add(qry[2]);
                 }
                 else
                 {
-                    lastAnswer = qry[2]%(list[(qry[1] ^ lastAnswer)%N].Count);
+                    //lastAnswer = qry[2]%(list[(qry[1] ^ lastAnswer)%N].Count);
+                    lastAnswer =  list[(qry[1] ^ lastAnswer)%N][qry[2]%(list[(qry[1] ^ lastAnswer)%N].Count)];
                     Console.WriteLine(lastAnswer);
                 }
                                 
             }
             
-           
-        }
     }
 }
